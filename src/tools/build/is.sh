2e5e661eff862dea5132630207a1f7c2761cbe48
Qu Jinping <jjqquu@gmail.com>
2017-08-08 16:22:18 +0800

    refine image stream of tools

--- src/tools/build/is.sh
+++ src/tools/build/is.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 
-oc import-image jenkins-2-centos7:latest --from=openshift/jenkins-2-centos7:latest --confirm
-oc import-image jenkins-slave-maven-centos7:latest --from=openshift/jenkins-slave-maven-centos7:latest --confirm
-oc import-image jenkins-slave-base-centos7:latest --from=openshift/jenkins-slave-base-centos7:latest --confirm
+oc import-image jenkins-2-centos7:latest --from=docker.io/openshift/jenkins-2-centos7:latest --confirm
+oc import-image jenkins-slave-maven-centos7:latest --from=docker.io/openshift/jenkins-slave-maven-centos7:latest --confirm
+oc import-image jenkins-slave-base-centos7:latest --from=docker.io/openshift/jenkins-slave-base-centos7:latest --confirm
--- src/tools/build/setup_acl.sh
+++ src/tools/build/setup_acl.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-# proj core
+# proj tools
 oadm policy add-role-to-group system:image-puller system:authenticated system:unauthenticated -n tools
