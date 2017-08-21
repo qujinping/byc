1082b559d3afc2aa28e6a8e7c75472663bfdc20c
Qu Jinping <jjqquu@gmail.com>
2017-08-21 15:32:36 +0800

    support cross-site deployment

--- src/sockstore/test/setup.sh
+++ src/sockstore/test/setup.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
 
-oc process template/jenkins-persistent -n openshift -p NAMESPACE=tools -p JENKINS_IMAGE_STREAM_TAG=jenkins-2-centos7:latest -p MEMORY_LIMIT=1024Mi | oc create -f - 
+oc process openshift/template/jenkins-persistent -p NAMESPACE=tools -p JENKINS_IMAGE_STREAM_TAG=jenkins-2-centos7:latest -p VOLUME_CAPACITY=2Gi -p MEMORY_LIMIT=1024Mi | oc create -f - 
 
 oc create --save-config -f jenkinscfg.yaml
 oc create --save-config -f int-test.yaml
+oc create --save-config -f remote-deploy.yaml
 
